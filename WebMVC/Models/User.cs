66e4c60d356053c1ffb36c8b355766f28743fd4a
TienN <tienn@tmtco.asia>
2022-05-25 09:16:21 +0700

    Add project files.

---
+++ WebMVC/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebMVC.Models
+{
+    public class User
+    {
+        public string? RequestId { get; set; }
+        public string? UserId { get; set; }
+        public string UserName { get; set; }
+        public int UserCount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
